59dcda235ed7babdc3f3b103a051bdbdb7071b58
Animesh Sinha <animeshsinha1309@gmail.com>
2020-02-11 14:48:53 +0530

    Gotta Catch 'em All
    Copied default code from a Pokemon API (https://github.com/NyaGarcia/pokeAPI). This time it's well written in TypeScript with Express and MongoDB.

---
+++ backend/src/app.ts
@@ -0,0 +1,36 @@
+import { Application } from 'express';
+import { Controller } from './main.controller';
+import { PokeService } from './services/pokemon.service';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
+
+
+class App {
+  public app: Application;
+  public pokeController: Controller;
+
+  constructor() {
+    this.app = express();
+    this._setConfig();
+    this._setMongoConfig();
+
+    this.pokeController = new Controller(this.app, new PokeService());
+  }
+
+  private _setConfig() {
+    this.app.use(bodyParser.json({ limit: '50mb' }));
+    this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+    this.app.use(cors());
+  }
+
+  private _setMongoConfig() {
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost:27017/Pokemon', {
+      useNewUrlParser: true
+    });
+  }
+}
+
+export default new App().app;
---
+++ backend/src/interfaces/pokemon-service.interface.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+
+export interface IPokemonService {
+  welcomeMessage(req: Request, res: Response): void;
+  getAllPokemon(req: Request, res: Response): void;
+  addNewPokemon(req: Request, res: Response): void;
+  deletePokemon(req: Request, res: Response): void;
+  updatePokemon(req: Request, res: Response): void;
+}
