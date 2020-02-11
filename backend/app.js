6ccd113e92603097064a2af16317b33a9e1565a4
Animesh Sinha <animeshsinha1309@gmail.com>
2020-02-11 10:48:48 +0530

    Copied Boilerplate
    Old JavaScript template code for CRUD applications from https://github.com/vigneshsithirai/crud_app, guide on https://scotch.io/@vigneshsithirai/basic-crud-operation-with-nodejs-express-and-mongodb.

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const http = require('http');
+//Models
+require('./server/model/user');
+
+//Routes
+var indexRouter = require('./server/routes/index');
+var usersRouter = require('./server/routes/users');
+
+//Configs
+require('./server/config/connection');
+
 
 var app = express();
 
@@ -20,15 +29,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,5 +46,20 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3100';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
 
 module.exports = app;
