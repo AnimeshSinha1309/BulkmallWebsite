cacd5ae932c7bae08969e332bf35da84d48d927d
Animesh Sinha <animeshsinha1309@gmail.com>
2020-02-13 11:51:07 +0530

    Added Login and Register
    No API yet, just some state and variables and the screen.

--- README.md
+++ README.md
@@ -12,18 +12,31 @@ Use the link here for how to install [MongoDB on any system](https://docs.mongod
 Following are the commands for installing on Ubuntu:
 
 `$ sudo apt-get install gnupg`
+
 `$ wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -`
+
 `$ echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list`
+
 `$ sudo apt-get update`
+
 `$ sudo apt-get install -y mongodb-org`
+
 `$ echo "mongodb-org hold" | sudo dpkg --set-selections`
+
 `$ echo "mongodb-org-server hold" | sudo dpkg --set-selections`
+
 `$ echo "mongodb-org-shell hold" | sudo dpkg --set-selections`
+
 `$ echo "mongodb-org-mongos hold" | sudo dpkg --set-selections`
+
 `$ echo "mongodb-org-tools hold" | sudo dpkg --set-selections`
+
 `$ sudo systemctl daemon-reload`
+
 `$ sudo systemctl start mongod`
+
 `$ sudo systemctl status mongod`
+
 `$ mongo`
 
 ### Install Express
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
+import Login from './components/Login';
+import Register from './components/Register';
+import Buy from './components/Buy';
+import Sell from './components/Sell';
+import Purchases from './components/Purchases';
+import Sales from './components/Sales';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          {/* Code for Navigation Topbar */}
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <NavLink to="/" exact={true} activeClassName='active' className="nav-link">Home</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/buy" activeClassName='active' className="nav-link">Buy</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/purchases" activeClassName='active' className="nav-link">Purchases</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/sell" activeClassName='active' className="nav-link">Sell</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/sales" activeClassName='active' className="nav-link">Sales</NavLink>
+                </li>
+              </ul>
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <NavLink to="/login" className="nav-link">Login</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/register" className="nav-link">Register</NavLink>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </header>
+        <main>
+          {/* Call the contents of different files using the Router */}
+          <Switch> <Route path="/login"><Login /></Route> </Switch>
+          <Switch> <Route path="/register"><Register /></Route> </Switch>
+          <Switch> <Route path="/buy"><Buy /></Route> </Switch>
+          <Switch> <Route path="/purchases"><Purchases /></Route> </Switch>
+          <Switch> <Route path="/sell"><Sell /></Route> </Switch>
+          <Switch> <Route path="/sales"><Sales /></Route> </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
